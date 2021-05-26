8633d7e2747ecdddf0ca2a3f451d1fa5b3489eca
Huang82 <90618109@gcloud.csu.edu.tw>
2021-05-26 13:57:41 +0800

    factorial 2021/5/26

---
+++ book/Recursion/factorial.java
@@ -0,0 +1,18 @@
+
+public class factorial {
+    public static void main(String[] args) {
+
+        System.out.println(cal(10));
+
+    }   
+    
+    private static int cal(int n) {
+        
+        if (n == 1) {
+            return 1;
+        } else {
+            return n * cal(n - 1);
+        }
+
+    } 
+}
